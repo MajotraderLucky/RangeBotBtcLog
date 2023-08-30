c9b61ad74ab742472eb0ff7f4813bb4034bf4fc7
Sergey Ryazanov <majo78@mail.ru>
2023-08-30 20:21:54 +0300

    feat(main): Change function Init; add account info

--- go.mod
+++ go.mod
@@ -4,5 +4,11 @@ go 1.20
 
 require (
 	github.com/MajotraderLucky/Utils v0.0.0-20230825021439-1d31827d98dd // indirect
+	github.com/adshao/go-binance/v2 v2.4.5 // indirect
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/MajotraderLucky/Utils/logger"
+	"github.com/adshao/go-binance/v2"
 	"github.com/joho/godotenv"
 )
 
@@ -12,6 +17,54 @@ func Init() {
 		log.Println("No.env file found.")
 	}
 	log.Println(".env file loaded.")
+
+	apiKey, exists := os.LookupEnv("BINANCE_API_KEY")
+	if !exists {
+		log.Fatal("BINANCE_API_KEY not set")
+	}
+
+	secretKey, exexists := os.LookupEnv("BINANCE_SECRET_KEY")
+	if !exexists {
+		log.Fatal("BINANCE_SECRET_KEY not set")
+	}
+
+	futuresClient := binance.NewFuturesClient(apiKey, secretKey)
+
+	accountBalance, err := futuresClient.NewGetAccountService().Do(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	accVar, _ := json.Marshal(accountBalance)
+
+	type Account struct {
+		FeeTier                     int    `json:"feeTier"`
+		CanTrade                    bool   `json:"canTrade"`
+		CanDeposit                  bool   `json:"canDeposit"`
+		CanWithdraw                 bool   `json:"canWithdraw"`
+		UpdateTime                  int64  `json:"updateTime"`
+		TotalInitialMargin          string `json:"totalInitialMargin"`
+		TotalMaintMargin            string `json:"totalMaintMargin"`
+		TotalWalletBalance          string `json:"totalWalletBalance"`
+		TotalUnrealizedProfit       string `json:"totalUnrealizedProfit"`
+		TotalMarginBalance          string `json:"totalMarginBalance"`
+		TotalPositionInitialMargin  string `json:"totalPositionInitialMargin"`
+		TotalOpenOrderInitialMargin string `json:"totalOpenOrderInitialMargin"`
+		TotalCrossWalletBalance     string `json:"totalCrossWalletBalance"`
+		TotalCrossUnPnl             string `json:"totalCrossUnPnl"`
+		AvailableBalance            string `json:"availableBalance"`
+		MaxWithdrawAmount           string `json:"maxWithdrawAmount"`
+	}
+
+	var acc Account
+	json.Unmarshal(accVar, &acc)
+	accountStart := 18.149229049682617 + 7.53667852 + 11.86 + 11.97 - 10
+	accountNowString := acc.AvailableBalance
+	if accountNowFloat, err := strconv.ParseFloat(accountNowString, 32); err == nil {
+		log.Println("-------------------Balance usdt----------------------")
+		log.Println(accountStart, "- start")
+		log.Println(accountNowFloat, "- now")
+		log.Println("-----------------------------------------------------")
+	}
 }
 
 func main() {
