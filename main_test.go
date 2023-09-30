aa980b9be2996e0f4319ff30acdb8b3f2b0ebc05
MajotraderLucky <majohead@gmail.com>
2023-09-30 20:56:05 +0300

    test(main_test): Add test for CreateLogsDir() func

--- main_test.go
+++ main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/MajotraderLucky/MarketRepository/initlog"
+	"github.com/MajotraderLucky/Utils/logger"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,4 +15,18 @@ func TestCheckFilesExist(t *testing.T) {
 
 	// Check that the function returns true
 	assert.True(t, result)
+}
+
+func TestCreateLogsDir(t *testing.T) {
+	logger := logger.Logger{}
+	err := logger.CreateLogsDir()
+	if err != nil {
+		t.Errorf("Expected no error, but got: %v", err)
+	}
+
+	// Check that the "logs" directory was created
+	_, err = os.Stat("logs")
+	if os.IsNotExist(err) {
+		t.Error("Expected 'logs' directory to be created, but it doesn't exist")
+	}
 }
